c1512ce08fa398aa40b56a47d9d1bfb278f48a51
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-05-06 12:26:09 +0100

    class work

---
+++ script.js
@@ -0,0 +1,88 @@
+console.log(200);
+
+
+// prompt()
+
+// let input = prompt("enter a name");
+// console.log(input);
+// let drama = prompt("login here");
+// alert("success");
+
+// let name = prompt("what is your name");
+// console.log(name);
+// alert("this is good");
+// let date = prompt("enter dateof birth");
+// console.log(date);
+// alert("good to go");
+// const number = prompt("enter digit");
+// console.log(number);
+// alert("invalid");
+
+
+
+
+
+//  Alert
+
+//alert("correct");
+//alert("all the best");
+//alert("welcome to my site");
+//alert("logged in successfully");
+//alert("login denied");
+
+// Backtick
+
+// let name = ["joy","tunde","bolu",];
+// let greetings = `welcome ${name} to the community `;
+// console.log(greetings);
+
+let number= "25";
+let digit = ` ${number} `;
+console.log(digit);
+
+    
+
+
+
+
+
+
+
+
+
+// let name = prompt("What is your name")
+// console.log(name)
+// alert("this is good");
+// const username = prompt("Enter your name");
+// alert(`welcome ${username}`);
+
+// let boxOne = prompt("Enter female numbers");
+// let boxTwo = prompt("Enter male numbers");
+// let ans = boxOne + boxTwo;
+// alert(ans)
+
+
+// let john = parseInt(prompt("Enter female numbers"));
+// let mary = parseInt(prompt("Enter male numbers"));
+// let you = john + mary;
+// alert(you)
+
+//Exercise list
+// 1. write a program that checks if the number is positive or negative
+
+// let number = -18
+// if (number >0) {
+// console.log ("this is a positive number")}
+// else{
+//     console.log("this is a negative number")
+// }
+
+
+//2.write a program that checks if the number is even or old
+// let num = parseInt(prompt("Enter a number"))
+// if (num % 2 === 0){
+//  alert(`${num} is an even number`)
+// }
+// else{
+//     alert(`${num} is an old number`)
+// }
