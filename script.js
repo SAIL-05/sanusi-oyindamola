0036db1ba4433f896f72b90ee4323cc47e1b723a
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-05-08 11:36:43 +0100

    added some new lessons

--- script.js
+++ script.js
@@ -77,22 +77,46 @@ console.log(200);
 //Exercise list
 // 1. write a program that checks if the number is positive or negative
 
-// let number = -18
-// if (number >0) {
+// let number = 0;
+// if (number >=0) {
 // console.log ("this is a positive number")}
 // else{
 // console.log("this is a negative number")
 // }
 
 
-//  2.write a program that checks if the number is even or old
+// let number = 7;
+// if(number >= 0){
+//     console.log("this is a positive number");
+// } else{
+//     console.log("this is a negative number");
+// }
+
+
+//  2.write a program that checks if the number is even or odd
 // let num = parseInt(prompt("Enter a number"))
 // if (num % 2 === 0){
 //  alert(`${num} is an even number`)
 // }
 // else{
-//     alert(`${num} is an old number`)
+//     alert(`${num} is an odd number`)
 // }
+
+
+// let num = parseInt(prompt("Enter a number"))
+// if(num >= 0){
+//     alert(`${num} is a positive number`);
+//  } else if(isNaN(num)){
+//     alert("enter a number");
+//  }
+//  else{
+//     alert(`${num} is a negative number`);
+// }
+
+
+
+
+
 
 // if else if conditional statement
 
@@ -100,66 +124,94 @@ console.log(200);
 // const userNumber = parseInt(prompt("Enter a number")); // prompts the user to enter a number
 
 // if (userNumber > 0) {
-//     alert(`The number ${userNumber} is a positive number`);
+//     alert(`${userNumber} is a positive number`);
 // }
 // else if (userNumber === 0) {
-//     alert(`The number ${userNumber} is invalid`);
+//     alert(`${userNumber} is invalid`);
 // }
 // else if(isNaN(userNumber)) {
-//     alert(`The number ${userNumber} is not a number`);
+//     alert("enter a number");
 // }
 
 // else if (userNumber.toString() === "NaN") {
 //   alert(`This is not a number`);
 // }
 // else {
-//     alert(`The number ${userNumber} is a negative number`);
+//     alert(`${userNumber} is a negative number`);
 // }
+//  let number = parseInt(prompt("Enter a number"))
+//  if(number > 0){
+//     alert(`${number}is a positive number`);
+//  }else if(number === 0){
+//     alert(`${number} is invalid`);
+//  }else if(isNaN(number)){
+//     alert("enter a number");
+//  }else{
+//     alert(`${number} is a negative number`);
+//  }
 
 // write a program that determines the greatest of two numbers.
-// let firstNumber = parseInt(prompt("Enter the first number"));
-// let secondNumber = parseInt(prompt("Enter the second number"));
+// let firstNumber = parseInt(prompt("Enter first number"))
+// let secondNumber = parseInt(prompt("Enter second number"))
 
-// if (firstNumber > secondNumber) {
-//     alert(`The first number ${firstNumber} is the greatest`);
-// } else if (secondNumber > firstNumber) {
-//     alert(`The second number ${secondNumber} is the greatest`);    
-// } 
-// else if (firstNumber == secondNumber) {
-//     alert(`Both numbers are equal`);
-// } 
-// else if(isNaN(firstNumber || secondNumber)) {
-//     alert(`The user didn't enter a number`);
+// if(firstNumber > secondNumber){
+//     alert("firstNumber is greater");
+// }else if(secondNumber > firstNumber){
+//     alert("secondNumber is the greatest");
+// }else{
+//     alert("invalid");
 // }
 
 
 
 // write a program that transforms a numerical grade to a letter grade 
 // (e.g grade 0-39 should display "F", 40-44 should display "E", 45-49 should display "D", 
-// 50-59 should display "C", 50-59 should display "B", 70-100 should display "A").
+// 50-59 should display "C", 60-69 should display "B", 70-100 should display "A").
 
-// let grade = parseInt(prompt("Enter your grade"));
-
-// if (grade >= 0 && grade <= 39) {
-//     alert(`Failed you got an F`);
+// let score = parseInt(prompt("Enter your score"))
+// if(score >=0 && score <= 39){
+//     alert(`${score} is Grade F`);
+// }else if(score >=40 && score <= 44){
+//     alert(`${score} is Grade E`);
+// }else if(score >=45 && score <= 49){
+//     alert(`${score} is Grade D`);
+// }else if(score >= 50 && score <= 59){
+//     alert(`${score} is Grade C`);
+// }else if(score >= 60 && score <= 69){
+//     alert(`${score} is Grade B`);
+// }else if(score >= 70 && score <= 100){
+//     alert(`${score} is Grade A`);
+// }else{
+//     alert("invalid");
 // }
 
-// else if (grade >= 40 && grade <= 44) {
-//     alert(`Your grade is an E`);
-// } else if (grade >= 45 && grade <= 49) {
-//     alert(`Your grade is an D`);
-// } else if (grade >= 50 && grade <= 59)3  {
-//     alert(`Your grade is an C`);
-// } else if (grade >= 60 && grade <= 69) {
-//     alert(`Your grade is an B`);
-// } else if (grade >= 70 && grade <= 100) {
-//     alert(`Your grade is an A`);
+
+// ASSIGNMENTS
+//  write a program that calculates the body max index (BMI) and categorizes it. The formular for BMI is:
+//  weight / (height * height) where weight is in kg and height is in meters. The categories are as follows:
+//  underweight (BMI < 18.5), normal weight (BMI 18.5 - 24.9), overweight (BMI 25 - 29.9), and obesity (BMI >= 30).
+
+// let bmi = parseInt(prompt("enter bmi here "));
+// if(bmi < 18.5){
+//     alert(`you are ${bmi} and underweight `);
+// } else if(bmi >= 18.5 && bmi < 24.9){
+//     alert(`you are ${bmi} a normal weight`);
+// } else if(bmi > 25 && bmi < 29.9){
+//     alert(`you are ${bmi} and over weight`);
+// }else if(bmi >= 30){
+//     alert(`you are ${bmi} and obessed`);
+// }
+// else{
+//     alert("invalid");
 // }
 
 
 
-// //ASSIGNMENTS
-// write a program that calculates the body max index (BMI) and categorizes it. The formular for BMI is:
-// weight / (height * height) where weight is in kg and height is in meters. The categories are as follows:
-// underweight (BMI < 18.5), normal weight (BMI 18.5 - 24.9), overweight (BMI 25 - 29.9), and obesity (BMI >= 30).
-
+// let number = parseInt(prompt("Enter a number"))
+// if(number >= 0){
+//     alert(`${number} is a positive number`);
+// } else if(number < 0){
+//     alert(`${number} is a negative number`);
+// }else{
+//     alert("invalid");
+// }
